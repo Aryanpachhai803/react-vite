415b51ad7e608218bc71f69b8fd4a8025c907d1b
Aryanpachhai <aryanpachhai803@gmail.com>
2026-06-02 14:21:57 +0545

    Chore: Initialize the react

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar.jsx"
+import Body from "./components/Body.jsx"
+import Footer from "./components/Footer.jsx"
+
+function App() {
+
+  return(
+    <div>
+      <Navbar />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Body.jsx
@@ -0,0 +1,11 @@
+function Body(){
+    return(
+        <div>
+            <h2>This is main body</h2>
+        </div>
+    );
+}
+
+export default Body;
+
+// rafce (creats default function format)
