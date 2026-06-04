3f27b7f8af6f826d89913f633487c70b395db565
Aryanpachhai <aryanpachhai803@gmail.com>
2026-06-04 14:11:39 +0545

    Initilzation through proucts constant

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,14 +1,17 @@
 import ProductCard from "./ProductCard.jsx";
 import "../styles/Body.css"
-import rickandmorty from "../assets/rickandmorty.png";
+import {products} from "../constants/constant.js"
 
 function Body(){
+    console.log(products);
     return(
         <main className="product-cards">
-            <ProductCard title="Air Jordan 4" description="Comfy shoes with cool design" price="$99" image={rickandmorty} />
+            {products.map((product) =>
+            <ProductCard data={product} key={product.id} />)}
+            {/* <ProductCard title="Air Jordan 4" description="Comfy shoes with cool design" price="$99" image={rickandmorty} />
             <ProductCard title="Adidas Ultraboost" description="Best for sports" price="$79" image={rickandmorty} />
             <ProductCard title="Nike Air Force 1" description="Streetwear" price="$89" image={rickandmorty} />
-            <ProductCard title="Loafers" description="Party shoe" price="$69" image={rickandmorty} />
+            <ProductCard title="Loafers" description="Party shoe" price="$69" image={rickandmorty} /> */}
         </main>
     );
 }
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,23 +1,23 @@
 import React from 'react'
 import "../styles/ProductCard.css"
 
-const ProductCard = (props) => {
-    console.log(props);
+const ProductCard = ({data}) => {
+    const {image,title,description,price}=data;
     return (
         <article className="product-card">
             <div className="product-card__image" aria-hidden="true">
-                <img src={props.image} alt="" />
+                <img src={image} alt="" />
             </div>
 
             <div className="product-card__content">
                 <p className="product-card__tag">New Arrival</p>
-                <h2 className="product-card__title">{props.title}</h2>
+                <h2 className="product-card__title">{title}</h2>
                 <p className="product-card__description">
-                    {props.description}
+                    {description}
                 </p>
 
                 <div className="product-card__footer">
-                    <span className="product-card__price">{props.price}</span>
+                    <span className="product-card__price">{price}</span>
                     <button className="product-card__button" type="button">
                         Add to Cart
                     </button>
