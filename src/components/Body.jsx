9b4b777ddd6605957d14f09c6583386d01612ce0
Aryanpachhai <aryanpachhai803@gmail.com>
2026-06-05 13:57:45 +0545

    Initialization for UI implement

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,12 +1,30 @@
 import ProductCard from "./ProductCard.jsx";
 import "../styles/Body.css"
 import {products} from "../constants/constant.js"
+import {useState} from "react";
 
 function Body(){
-    console.log(products);
+    const [productList, setProductList] = useState(products);
+    const [element,setElement] = useState(4);
+    
+    
+    function handleTopRatedProducts(){
+        const topRatedProducts = products.filter((product) => product.rating.rate >= 4);
+        setProductList(topRatedProducts);
+        console.log(topRatedProducts);
+    }
+    function handleAdd(){
+        const add = element+1;
+        setElement(add);
+    }
     return(
         <main className="product-cards">
-            {products.map((product) =>
+            <div>
+                <button onClick={handleTopRatedProducts}>Top Rated Products</button>
+                <button onClick={handleAdd}>Add</button>
+                <h2>{element}</h2>
+            </div>
+            {productList.map((product) =>
             <ProductCard data={product} key={product.id} />)}
             {/* <ProductCard title="Air Jordan 4" description="Comfy shoes with cool design" price="$99" image={rickandmorty} />
             <ProductCard title="Adidas Ultraboost" description="Best for sports" price="$79" image={rickandmorty} />
@@ -19,3 +37,4 @@ function Body(){
 export default Body;
 
 // rafce (creats default function format)
+
